3ea55627a9c49de438376b64765f918e75a04d22
Shubham Kumar <65327511+shubhamkr790@users.noreply.github.com>
2026-07-28 15:30:55 +0530

    Create War_Card_GameApp.swift

---
+++ War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Shubham on 28/07/26.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
